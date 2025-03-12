505ab99dead0c6885054f499f22cb92a7a66b0ff
Luis Theml <44423391+Theml@users.noreply.github.com>
2025-03-12 08:56:33 -0300

    Update LoggersTest.java

--- LoggersTest.java
+++ LoggersTest.java
@@ -1,2 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class LoggersTest {
+
+  @Test
+  public void deveRetornarArquivoEscrito() {
+    Loggers.getInstance().setArquivoEscrito("Arquivo 1");
+    assertEquals("Arquivo 1", Loggers.getInstance().getArquivoEscrito());
+  }
+
+  @Test
+  public void deveRetornarMensagemEnviada() {
+    Loggers.getInstance().setMensagemEnviada("Mensagem Teste");
+    assertEquals("Mensagem Teste", Loggers.getInstance().getMensagemEnviada());
+  }
 }
